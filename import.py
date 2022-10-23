1b37b6d0ccfab6358c2d71a62d33dc3d2abf9708
Luke Blaney <git@lukeblaney.co.uk>
2022-10-23 14:58:08 +0100

    Refactor accounts logic and remove writing back to people API
    Rather than handling email, phones etc independently, start treating them all as "accounts", as per lucos_contacts.
    Also, don't write the externalID back to google here - that logic should live in an lucos export script, rather than import.

--- import.py
+++ import.py
@@ -10,7 +10,7 @@ creds = service_account.Credentials.from_service_account_info(
 			"private_key": os.environ.get('PRIVATE_KEY'),
 			"client_email": os.environ.get('CLIENT_EMAIL'),
 			"token_uri": "https://oauth2.googleapis.com/token",
-		}, scopes=['https://www.googleapis.com/auth/contacts'], subject=os.environ.get('USER_EMAIL'))
+		}, scopes=['https://www.googleapis.com/auth/contacts.readonly'], subject=os.environ.get('USER_EMAIL'))
 
 LUCOS_CONTACTS = os.environ.get('LUCOS_CONTACTS')
 if not LUCOS_CONTACTS:
@@ -18,37 +18,20 @@ if not LUCOS_CONTACTS:
 
 LUCOS_HEADERS={'AUTHORIZATION':"key "+os.environ.get('LUCOS_CONTACTS_API_KEY')}
 
-EXTERNAL_ID_TYPE='lucos' # Used for storing & retreiving lucos contact ids in Google's people API as an external ID
+EXTERNAL_ID_TYPE='lucos' # Used for retreiving lucos contact ids in Google's people API as an external ID
 
-# Search for an existing match in lucos, starting with phone numbers, then email and falling back to names
-# TODO: once Google's People API IDs are stored in lucos, use those with highest priority
-# (Currently lucos stores the Google's Contact API IDs, but Google didn't make their IDs backwards compatible, because that'd be too useful)
+# Search for an existing match in lucos, based on the order of items in the accounts arround
 #
 # Returns the agentid as a string, if a match is found.  Otherwise returns None
-def matchContact(data):
-	for number in data['phoneNumbers']:
-		resp = requests.get(LUCOS_CONTACTS+"identify", headers=LUCOS_HEADERS, params={'type':'phone','number':number}, allow_redirects=False)
+def matchContact(accounts, primaryName):
+	for account in accounts:
+		resp = requests.get(LUCOS_CONTACTS+"identify", headers=LUCOS_HEADERS, params=account, allow_redirects=False)
 		if resp.status_code == 302:
 			return resp.headers['Location'].replace("/agents/","")
 		if resp.status_code == 409:
-			print("Conflict for "+data['primaryName']+" - "+number)
+			print("Conflict for "+primaryName+" - "+account['type'])
 		if resp.status_code >= 500:
 			resp.raise_for_status()
-	for address in data['emailAddresses']:
-		resp = requests.get(LUCOS_CONTACTS+"identify", headers=LUCOS_HEADERS, params={'type':'email','address':address}, allow_redirects=False)
-		if resp.status_code == 302:
-			return resp.headers['Location'].replace("/agents/","")
-		if resp.status_code == 409:
-			print("Conflict for "+data['primaryName']+" - "+address)
-		if resp.status_code >= 500:
-			resp.raise_for_status()
-	resp = requests.get(LUCOS_CONTACTS+"identify", headers=LUCOS_HEADERS, params={'type':'name','name':data['primaryName']}, allow_redirects=False)
-	if resp.status_code == 302:
-		return resp.headers['Location'].replace("/agents/","")
-	if resp.status_code == 409:
-		print("Conflict for "+data['primaryName']+" - name")
-	if resp.status_code >= 500:
-		resp.raise_for_status()
 	return None
 
 def newContact(name):
@@ -77,68 +60,70 @@ try:
 		).execute()
 		for (resourceName, data) in zip(next200, people['responses']):
 			person = data['person']
-			output = {
-				'primaryName': 'Unknown Google Contact',
-				'phoneNumbers': [],
-				'emailAddresses': [],
+
+			birthday = {
+				'day': None,
+				'month': None,
+				'year': None,
 			}
-			for name in person['names']:
+			for birthday_instance in person.get('birthdays',[]):
+				if 'day' in birthday_instance['date']:
+					birthday['day'] = birthday_instance['date']['day']
+				if 'month' in birthday_instance['date']:
+					birthday['month'] = birthday_instance['date']['month']
+				if 'year' in birthday_instance['date']:
+					birthday['year'] = birthday['date']['year']
+
+			photos = {'CONTACT': None, 'PROFILE': None}
+			for photo in person.get('photos',[]):
+				if 'default' not in photo:
+					photos[photo['metadata']['source']['type']] = photo['url']
+			# Prefer photo I've set, but default to their profile pic otherwise
+			photoUrl = photos['CONTACT'] or photos['PROFILE']
+
+
+
+			## Add items to the accounts list in the order of precedence to find matches
+			accounts = []
+			accounts.append({
+				"type": "googlecontact",
+				"contactid": resourceName,
+			});
+			for num in person.get('phoneNumbers',[]):
+				accounts.append({
+					"type": "phone",
+					"number": num['canonicalForm'],
+				})
+			for email in person.get('emailAddresses',[]):
+				accounts.append({
+					"type": "email",
+					"address": email['value'],
+				})
+			primaryName = 'Unknown Google Contact'
+			for name in person.get('names', []):
 				if name['metadata']['primary']:
-					output['primaryName'] = name['displayName']
-			if 'emailAddresses' in person:
-				output['emailAddresses'] = [email['value'] for email in person['emailAddresses']]
-			if 'phoneNumbers' in person:
-				output['phoneNumbers'] = [num['canonicalForm'] for num in person['phoneNumbers']]
-			if 'birthdays' in person:
-				day = None
-				month = None
-				year = None
-				for birthday in person['birthdays']:
-					if 'day' in birthday['date']:
-						day = birthday['date']['day']
-					if 'month' in birthday['date']:
-						month = birthday['date']['month']
-					if 'year' in birthday['date']:
-						year = birthday['date']['year']
-				output['birthday'] = {
-					day: day,
-					month: month,
-					year: year,
-				}
-			if 'photos' in person:
-				photos = {'CONTACT': None, 'PROFILE': None}
-				for photo in person['photos']:
-					if 'default' not in photo:
-						photos[photo['metadata']['source']['type']] = photo['url']
-				# Prefer photo I've set, but default to their profile pic otherwise
-				output['photoUrl'] = photos['CONTACT'] or photos['PROFILE']
+					primaryName = name['displayName']
+				accounts.append({
+					"type": "name",
+					"name": name['displayName'],
+				})
 
-			print(output)
+			print(primaryName, accounts)
 
-			existingcontactid = None
+			contactid = None
 			externalIds = person.get('externalIds', [])
 			for externalId in externalIds:
 				if externalId['type'] == EXTERNAL_ID_TYPE:
-					existingcontactid = externalId['value']
-
-			contactid = existingcontactid
+					contactid = externalId['value']
 			if not contactid:
 				print("No existing lucos ID found for contact, trying to match against existing lucos contacts...")
-				contactid = matchContact(output)
+				contactid = matchContact(accounts, primaryName)
 			if not contactid:
 				print("No matching lucos contact found, creating new one...")
-				contactid = newContact(output['primaryName'])
+				contactid = newContact(primaryName)
 			print(contactid or "NOT FOUND")
 
-			if not existingcontactid:
-				externalIds.append({'type':EXTERNAL_ID_TYPE, 'value': contactid})
-				contactsToUpdate[resourceName] = {'metadata': person['metadata'], 'externalIds': externalIds}
-		if contactsToUpdate:
-			print("update lucos ids in google", contactsToUpdate)
-			service.people().batchUpdateContacts(body={
-				"contacts": contactsToUpdate,
-				"updateMask": "externalIds",
-			}).execute()
+
 
 except HttpError as err:
 	print(err)
