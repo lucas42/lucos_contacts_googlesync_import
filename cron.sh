1f80406643454bd04434235c09994384b0ef657d
Luke Blaney <git@lukeblaney.co.uk>
2024-01-10 01:26:16 +0000

    printenv --null doesn't seem to work on alpine; try env -0 instead

--- cron.sh
+++ cron.sh
@@ -3,7 +3,7 @@ set -e
 
 # printenv doesn't quote values, which is a problem if one contains a space or newline
 # So do some hacky regexes to quote stuff
-printenv --null | sed 's/"/\\"/g' | sed -z "s/\n/\\\\n/g" | sed 's/\x0/\n/g'| sed 's/=/="/' | sed 's/$/"/g' | sed 's/\\n/\n/g' > .env
+env -0 | sed 's/"/\\"/g' | sed -z "s/\n/\\\\n/g" | sed 's/\x0/\n/g'| sed 's/=/="/' | sed 's/$/"/g' | sed 's/\\n/\n/g' > .env
 [ -p /var/log/cron.log ] || mkfifo /var/log/cron.log
 /usr/sbin/crond
 cat <> /var/log/cron.log
